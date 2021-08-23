53dd31a0bd9308cb48dec6e659180397bea485bf
Alen Varazdinac <alen.varazdinac@gmail.com>
2021-08-23 11:13:43 +0200

    feat: next slide control

--- src/js/main.js
+++ src/js/main.js
@@ -79,7 +79,39 @@ $(() => {
 
   // Next slide
   $('#nextSlideControl').on('click', () => {
-    console.log('next slide', Math.ceil(images.length / 2))
+    // Get last image from top row and its width
+    const topRowLastImage = $('.slider__wrapper--top').find('img:last')
+    const topRowLastImageWidth = topRowLastImage.width()
+
+    // Get last image from bottom row and its width
+    const bottomRowLastImage = $('.slider__wrapper--bottom').find(
+      '.image-position-bottom:last'
+    )
+    const bottomRowLastImageWidth = bottomRowLastImage.width()
+
+    $('.slider__wrapper--top').animate(
+      { marginLeft: +topRowLastImageWidth },
+      {
+        duration: 1000,
+        queue: false,
+        complete: () => {
+          $('.slider__wrapper--top').prepend(topRowLastImage)
+          $('.slider__wrapper--top').css({ marginLeft: 0 })
+        }
+      }
+    )
+
+    $('.slider__wrapper--bottom').animate(
+      { marginLeft: +bottomRowLastImageWidth },
+      {
+        duration: 1000,
+        queue: false,
+        complete: () => {
+          $('.slider__wrapper--bottom').prepend(bottomRowLastImage)
+          $('.slider__wrapper--bottom').css({ marginLeft: 0 })
+        }
+      }
+    )
   })
 })
 
